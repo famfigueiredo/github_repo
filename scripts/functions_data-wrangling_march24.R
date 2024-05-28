86408c2a69c5a2e973dce946187374bc3c140ede
Filipe Figueiredo <filipe.figueiredo@uit.no>
2024-05-28 14:41:43 +0200

    added gsea formatting functions

--- scripts/functions_data-wrangling_march24.R
+++ scripts/functions_data-wrangling_march24.R
@@ -115,6 +115,101 @@ filter_rows_by_GO_term <- function(df1, df2, id_column_name) {
   return(filtered_rows)
 }
 
+# gsea formatting starting from a DESeq results table
+gsea_formatting <- function(results_table, treatment, sampling_point) {
+  # Install and load required packages
+  required_packages <- c('dplyr', 'gprofiler2', 'clusterProfiler', 'org.Hs.eg.db')
+  installed_packages <- rownames(installed.packages())
+  
+  for (pkg in required_packages) {
+    if (!(pkg %in% installed_packages)) {
+      install.packages(pkg)
+    }
+    library(pkg, character.only = TRUE)
+  }
+  
+  # Convert rownames to column 'ensembl'
+  results_df <- tibble::rownames_to_column(as.data.frame(results_table), var = 'ensembl')
+  
+  # Convert salmon genes to human orthologs
+  orthologs <- gorth(
+    query = rownames(results_table),
+    source_organism = 'ssalar',
+    target_organism = 'hsapiens',
+    mthreshold = 1,
+    filter_na = TRUE
+  )
+  
+  # Select relevant variables and join with ortholog data
+  merged_df <- results_df %>%
+    left_join(orthologs, by = c('ensembl' = 'input')) %>%
+    dplyr::select(
+      ensembl,
+      ortholog_name,
+      ortholog_ensg,
+      log2FoldChange,
+      padj,
+      description
+    ) %>%
+    na.omit()
+  
+  # Order genes by fold change
+  ordered_df <- merged_df[order(-merged_df$log2FoldChange), ]
+  
+  # Prepare matrix for GSEA
+  gene_list <- ordered_df$log2FoldChange
+  names(gene_list) <- ordered_df$ortholog_name
+  
+  # Run GSEA
+  gsea_results <- gseGO(
+    gene_list,
+    keyType = 'SYMBOL',
+    OrgDb = org.Hs.eg.db,
+    ont = 'BP',
+    pvalueCutoff = 0.05,
+    pAdjustMethod = 'BH',
+    verbose = T,
+    eps = 1e-300
+  )
+  
+  # Assign the results to a variable including treatment and sampling_point in the name
+  results_name <- paste0('gsea_results_', treatment, '_', sampling_point)
+  assign(results_name, gsea_results, envir = .GlobalEnv)
+  
+  # Print GSEA results
+  print(gsea_results)
+  
+  return(gsea_results)
+}
+
+# gsea formatting starting from a DESeq results table and using only significantly differentially regulated genes
+gsea_formatting_significant <- function(results_table, treatment, sampling_point) {
+  # Wrangle the data using the improved_data_wrangling function
+  wrangled_data <- improved_data_wrangling(results_table, treatment, sampling_point)
+  
+  # Select ortholog_name and log2FC, and remove NA values
+  ortholog_fc <- wrangled_data %>%
+    dplyr::select(ortholog_name, log2FC) %>%
+    na.omit()
+  
+  # Convert gene symbols to Entrez IDs
+  entrez_ids <- bitr(ortholog_fc$ortholog_name, fromType = 'SYMBOL', toType = 'ENTREZID', OrgDb = org.Hs.eg.db)
+  
+  # Merge ortholog_fc with entrez_ids and select relevant columns
+  enrichment <- ortholog_fc %>%
+    left_join(entrez_ids, by = c('ortholog_name' = 'SYMBOL')) %>%
+    dplyr::select(ENTREZID, log2FC)
+  
+  # Prepare the enrichment_gsea matrix
+  enrichment_gsea <- enrichment$log2FC
+  names(enrichment_gsea) <- enrichment$ENTREZID
+  
+  # Create a dynamic name for the enrichment_gsea object
+  results_name <- paste0("enrichment_gsea_", treatment, "_", sampling_point)
+  assign(results_name, enrichment_gsea, envir = .GlobalEnv)
+  
+  return(enrichment_gsea)
+}
 
 # Helper function to display Venn diagram
 display_venn <- function(x, ...){
